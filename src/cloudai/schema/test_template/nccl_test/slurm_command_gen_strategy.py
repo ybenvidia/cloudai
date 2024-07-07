5c7b93bf936ce646ca6acb7c9594c122084d981b
ybenvidia <ybenabou@nvidia.com>
2024-07-07 05:45:13 -0700

    add gres-gpu tests

--- src/cloudai/schema/test_template/nccl_test/slurm_command_gen_strategy.py
+++ src/cloudai/schema/test_template/nccl_test/slurm_command_gen_strategy.py
@@ -89,6 +89,7 @@ class NcclTestSlurmCommandGenStrategy(SlurmCommandGenStrategy):
 
         srun_command_parts = [
             f"srun --ntasks-per-node={cmd_args['ntasks_per_node']} ",
+            f"--gres=gpu:{cmd_args['gpus_per_node']} ",
             "--mpi=pmix",
             f"--container-image=/raid/nccl-test/nccl_test.sqsh",
         ]
