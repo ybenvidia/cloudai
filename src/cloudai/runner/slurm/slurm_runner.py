e807230a3723575afa64cb72d8a2921cf55cee7e
ybenvidia <ybenabou@nvidia.com>
2024-07-11 01:31:50 -0700

    fix

--- src/cloudai/runner/slurm/slurm_runner.py
+++ src/cloudai/runner/slurm/slurm_runner.py
@@ -83,7 +83,7 @@ class SlurmRunner(BaseRunner):
 
     def get_max_job_id(self):
         try:
-            squeue_output, _ = self.fetch_command_output("squeue --me --noheader --format %A")
+            squeue_output, _ = self.cmd_shell.execute("squeue --me --noheader --format %A").communicate()
             job_ids = squeue_output.strip().split()
             job_ids = [int(job_id) for job_id in job_ids if job_id.isdigit()]
             if job_ids:
