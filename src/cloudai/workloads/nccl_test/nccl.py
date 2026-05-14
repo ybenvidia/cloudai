1b85a6e3bad75226f0f3dd459c1b44fee5b3c483
ybenabou <ybenabou@nvidia.com>
2026-05-14 16:51:29 +0300

    add ucc/nccl all2allv

--- src/cloudai/workloads/nccl_test/nccl.py
+++ src/cloudai/workloads/nccl_test/nccl.py
@@ -101,6 +101,8 @@ class NCCLCmdArgs(CmdArgs):
     blocking: Union[int, list[int]] = 0
     cudagraph: Union[int, list[int]] = 0
     stepfactor: Optional[Union[int, list[int]]] = None
+    use_deepep_matrix: bool = False
+    alltoallv_matrix_container_path: str = "/tmp/traffic_matrix.txt"
 
 
 class NCCLTestDefinition(TestDefinition):
